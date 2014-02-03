dde4a653f7ffe4a7a220d1a3655491909ad6cc7c
Lauren E Krueger <laurenekrueger@Laurens-MacBook-Pro.local>
2014-02-03 14:54:15 +1100

    empty rails app for forgetmeknot

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+AlzheimersApp::Application.config.secret_key_base = '68498807723f2efcfe6fbd4e66c29b9aa00664b6dcc8528b57ec19bd865200bf41ea0237fe5b1494718e0332318c05eaae04f1db2b2f9eb70d095b12e40f3a74'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+AlzheimersApp::Application.config.session_store :cookie_store, key: '_alzheimers_app_session'
