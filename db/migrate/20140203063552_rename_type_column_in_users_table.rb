e84717ff669819393d370c422a79afa183a67090
Lauren E Krueger <laurenekrueger@Laurens-MacBook-Pro.local>
2014-02-07 13:50:38 +1100

    commenting out add migration for heroku

--- db/migrate/20140203063552_rename_type_column_in_users_table.rb
+++ db/migrate/20140203063552_rename_type_column_in_users_table.rb
@@ -1,7 +1,7 @@
 class RenameTypeColumnInUsersTable < ActiveRecord::Migration
   def change
     change_table :users do |t|
-      t.remove :type
+      # t.remove :type
       t.string :user_type
     end
   end
