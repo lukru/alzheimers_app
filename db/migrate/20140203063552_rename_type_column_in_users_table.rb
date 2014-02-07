bf945335adac4ab8b8f8e749d54f45339a6ecdd2
Lauren E Krueger <laurenekrueger@Laurens-MacBook-Pro.local>
2014-02-07 13:52:51 +1100

    commented out user type in rename migration

--- db/migrate/20140203063552_rename_type_column_in_users_table.rb
+++ db/migrate/20140203063552_rename_type_column_in_users_table.rb
@@ -2,7 +2,7 @@ class RenameTypeColumnInUsersTable < ActiveRecord::Migration
   def change
     change_table :users do |t|
       # t.remove :type
-      t.string :user_type
+      #t.string :user_type
     end
   end
 end
